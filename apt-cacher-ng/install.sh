e7e29e393892ef98830f4e54083ce128400d1a20
zubatyuk <zubatyuk@gmail.com>
2015-10-12 13:20:07 -0500

    Update install.sh

--- apt-cacher-ng/install.sh
+++ apt-cacher-ng/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPTDIR=$(readlink -f ${0%/*})
+SCRIPTDIR=$(dirname $(readlink -f ${0%/*}))
 
-sed -ie /%INSTALLDIR%/$SCRIPTDIR/ $SCRIPTDIR/docker-apt-cacher-ng.conf
+sed -ie "s|%INSTALLDIR%|$SCRIPTDIR|g" $SCRIPTDIR/docker-apt-cacher-ng.conf
 install $SCRIPTDIR/docker-apt-cacher-ng.conf /etc/init
 service docker-apt-cacher-ng start
