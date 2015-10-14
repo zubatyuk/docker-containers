8483b349f4a7e1d38224c9738618dd69d7301d72
zubatyuk <zubatyuk@gmail.com>
2015-10-13 20:49:35 -0500

    Delete install.sh

--- dnsmasq/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPTDIR=$(dirname $(readlink -f ${0%/*}))
-NAME=dnsmasq
-
-sed -ie "s|%INSTALLDIR%|$SCRIPTDIR|g" $SCRIPTDIR/docker-${NAME}.conf
-install $SCRIPTDIR/docker-${NAME}.conf /etc/init
-service docker-${NAME} start
