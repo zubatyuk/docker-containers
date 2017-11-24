e77a8631037781a1abf408165cbe2f7e9c4e5bc8
zubatyuk <zubatyuk@gmail.com>
2017-11-24 17:00:31 -0600

    Update aptproxy.sh

--- aptproxy.sh
+++ aptproxy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-APTPROXY=aptproxy
-PORT=3142
+set -e
+
+: ${PROXY_HOST:=aptproxy}
+: ${PROXY_PORT:=3142}
 
 if [ "$1" == "clean" ]; then
     rm /etc/apt/apt.conf.d/50squid-deb-proxy
     exit 0
 fi
 
-nc -z $APTPROXY $PORT
+nc -z $PROXY_HOST $PROXY_PORT
 if [  $? -eq 0 ]; then
-    echo "Acquire::http::proxy \"http://${APTPROXY}:${PORT}\";" > /etc/apt/apt.conf.d/50squid-deb-proxy
+    echo "Acquire::http::proxy \"http://${PROXY_HOST}:${PROXY_PORT}\";" > /etc/apt/apt.conf.d/50squid-deb-proxy
     echo "Acquire::https::proxy \"DIRECT\";" >> /etc/apt/apt.conf.d/50squid-deb-proxy
 fi
